f91386f01f44ce1560a4d9eb24d797e19dd5f3e3
adriano <adriano20fatec@gmail.com>
2020-07-08 20:37:29 -0300

    update

--- teste.php
+++ teste.php
@@ -18,7 +18,7 @@ if(isset($_GET['busca'])){
 <p class='card-text'>Marca: ". $dados['marca']."</p>
 <p class='card-text'>Preço: R$". $dados['preco']."</p>
 <p class='card-text'>Data de cadastro: ". $dados['dtaCadastro']."</p>
-<a  href='' class='btn btn-info'>Comprar</a>
+
 </div>
 </div>
 </div>";
